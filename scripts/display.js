04ca84b69c23405063f0b3e202d4cef3afb81963
Dan Yoo <dan.yoo@outlook.com>
2020-11-13 00:38:41 -0800

    added  updateprofile.html, profile.html, and profile.js

--- scripts/display.js
+++ scripts/display.js
@@ -3,7 +3,7 @@ let navStart = '<nav class="navbar navbar-expand-lg navbar-dark nav-bg">'
 	+ '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">'
 	+ '<span class="navbar-toggler-icon"></span></button>'
 	+ '<div class="collapse navbar-collapse" id="navbarTogglerDemo02">'
-	+ '<ul class="navbar-nav ml-auto">'; 
+	+ '<ul class="navbar-nav ml-auto">';
 
 let navEnd = '</ul></div></nav>';
 
@@ -13,15 +13,16 @@ let noLoginNav = navStart
 	+ '<li class="nav-item"><a class="nav-link" href="#">Settings</a></li>'
 	+ navEnd;
 
-firebase.auth().onAuthStateChanged(function(user) {
-	let loginNav = navStart
-		+ '<li class="nav-item"><a class="nav-link" href="login.html">Signed in as ' + user.displayName + '</a></li>'
-		+ '<li class="nav-item"><a class="nav-link" href="main.html">QR Scanner</a></li>'
-		+ '<li class="nav-item"><a class="nav-link" href="#">Settings</a></li>'
-		+ navEnd;
-
+firebase.auth().onAuthStateChanged(function (user) {
 	if (user) {
 		// User is signed in.
+		let loginNav = navStart
+			+ '<li class="nav-item"><a class="nav-link" href="login.html">Signed in as ' + user.displayName + '</a></li>'
+			+ '<li class="nav-item"><a class="nav-link" href="main.html">QR Scanner</a></li>'
+			+ '<li class="nav-item"><a class="nav-link" href="#">Settings</a></li>'
+			+ '<li class="nav-item"><a class="nav-link" href="#">Settings</a></li>'
+			+ navEnd;
+
 		$("nav").remove()
 		$(document.body).prepend(loginNav);
 	} else {
---
+++ scripts/profile.js
@@ -0,0 +1,5 @@
+function getProfilePic() {
+    $("#profile-img-cropper img").attr("src", "https://dummyimage.com/300x300/999999/ffffff&text=Not+Found");
+}
+
+getProfilePic();
