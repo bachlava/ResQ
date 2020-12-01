821af19cf53b228ef5a97b988005ad9d4d7157f9
Bach Le <bachcyp@gmail.com>
2020-12-01 10:15:58 -0800

    formatted and commented updateprofile.js

--- scripts/updateprofile.js
+++ scripts/updateprofile.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
+	var user = firebase.auth().currentUser;
 
-    var user = firebase.auth().currentUser;
-
-    $('button[type="submit"]').click(function () {
-        var newName = document.getElementById('updatedUserName').value;
-        user.updateProfile({
-            displayName: newName
-        })
-            .then(function () {
-                console.log("Profile Updated Successfully!");
-                console.log(newName);
-            })
-            .catch(function (error) {
-                console.error("Error writing document: ", error);
-            });
-        db.collection("users").doc(user.uid).set({
-            displayName: newName
-
-        }); 
-    })
+	$('button[type="submit"]').click(function () {
+		var newName = document.getElementById('updatedUserName').value;
+		
+		user.updateProfile({
+			displayName: newName
+		}).then(function () {
+			console.log("Profile Updated Successfully!");
+			console.log(newName);
+		}).catch(function (error) {
+			console.error("Error writing document: ", error);
+		});
+		
+		/* Updates the users name in Firestore. */
+		db.collection("users").doc(user.uid).set({
+			displayName: newName
+		}); 
+	});
 });
